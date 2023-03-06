f0906aab5b053a73121a26c853ad8c6e1f90c109
Sakchyam-Nakarmi <nsakchyam21@tbc.edu.np>
2023-03-06 21:08:08 +0545

    initial commit for xml layouts

---
+++ app/src/main/java/com/example/lab_2tipcalculator/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.lab_2tipcalculator
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.math.roundToInt
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
